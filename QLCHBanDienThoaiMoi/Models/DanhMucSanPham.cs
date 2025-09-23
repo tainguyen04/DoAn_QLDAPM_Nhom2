c253d474740cf8e7728964b2855d33166e9c8498
tainguyen04 <huutai18102004@gmail.com>
2025-09-23 15:27:02 +0700

    Chuyen file ra ngoai va xoa thu muc QLCHBanDienThoaiMoi

--- QLCHBanDienThoaiMoi/Models/DanhMucSanPham.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QLCHBanDienThoaiMoi.Models
-{
-    public class DanhMucSanPham
-    {
-        public int Id { get; set; }
-        public string? TenDanhMuc { get; set; }
-        public ICollection<SanPham> SanPham { get; set; }
-    }
-}
