e83e939b6ea09d017e1c9c532616641228d18540
tainguyen04 <huutai18102004@gmail.com>
2025-09-23 15:45:56 +0700

    Khoi phuc QLCHBanDienThoaiMoi tu branch Tai

---
+++ QLCHBanDienThoaiMoi/Models/ChiTietHoaDonNhap.cs
@@ -0,0 +1,13 @@
+﻿namespace QLCHBanDienThoaiMoi.Models
+{
+    public class ChiTietHoaDonNhap
+    {
+        public int HoaDonNhapId {get;set; }
+        public HoaDonNhap HoaDonNhap { get; set; }
+        public int SanPhamId { get; set; }
+        public SanPham SanPham { get; set; }
+        public int SoLuong { get; set; }
+        public int GiaNhap { get; set; }
+        
+    }
+}
---
+++ QLCHBanDienThoaiMoi/Models/SanPham.cs
@@ -0,0 +1,19 @@
+﻿namespace QLCHBanDienThoaiMoi.Models
+{
+    public class SanPham
+    {
+        
+        public int Id { get;set; }
+        public string TenSanPham { get; set; }
+        public int GiaNhap { get; set; }
+        public int GiaBan { get; set; }
+        public int SoLuongTon { get; set; }
+        public int DanhMucId { get; set; }
+        public DanhMucSanPham DanhMucSanPham { get; set; }
+        public string HangSanXuat { get; set; }
+        public string MoTa { get; set; }
+        public decimal KhuyenMai { get; set; } = 0;
+        public ICollection<ChiTietHoaDonNhap> ChiTietHoaDonNhap { get; set; }
+        public ICollection<ChiTietHoaDonBan> ChiTietHoaDonBan { get; set; }
+    }
+}
