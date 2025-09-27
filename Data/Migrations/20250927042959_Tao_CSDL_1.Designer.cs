d0c59b883eecf87d816b4698eb85b6c12221dc4c
tainguyen04 <huutai18102004@gmail.com>
2025-09-27 11:33:12 +0700

    Cap nhat lai Appsettings.json

--- Data/Migrations/20250927042959_Tao_CSDL_1.Designer.cs
+++ Data/Migrations/20250927042959_Tao_CSDL_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QLCHBanDienThoaiMoi.Data;
 
@@ -11,9 +12,11 @@ using QLCHBanDienThoaiMoi.Data;
 namespace QLCHBanDienThoaiMoi.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250927042959_Tao_CSDL_1")]
+    partial class Tao_CSDL_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
